3ad8d3198723d824664b58b9f049d45c33dc1a86
silviadelya <silviadelyaheryani31@gmail.com>
2021-10-24 15:55:52 +0700

    File Python

---
+++ latihan.py
@@ -0,0 +1,18 @@
+print("Latihan Python")
+print("Program Input Biodata")
+nama_lengkap=input("Nama Lengkap:")
+nama_panggilan=input("Nama Panggilan:")
+nim=input("NIM:")
+tempat_lahir=input("Tempat Lahir:")
+tanggal_lahir=input("Tanggal Lahir:")
+alamat=input("Alamat:")
+telpon=input("Nomor Telepon:")
+print()
+print("Assalamu'alaikum")
+print()
+print("Let me introduce my self. My name is",nama_lengkap,", but you can call me",nama_panggilan,
+      ". My NIM is",nim,". I was born in",tempat_lahir,tanggal_lahir,
+      ". I am very glad if you want to invite my house in",alamat,
+      ". So, don't forget to call me before with the number",telpon,)
+print()
+print("Thank You")
